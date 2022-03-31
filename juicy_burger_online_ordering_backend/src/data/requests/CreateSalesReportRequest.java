6c6ebd709fa24b73c3eb4aaff37845518b0489a0
Alexander Lucas Elia <alexanderlucaselia@gmail.com>
2022-03-31 00:18:51 -0700

    update 2 of CreateSalesReportActivity

--- juicy_burger_online_ordering_backend/src/data/requests/CreateSalesReportRequest.java
+++ juicy_burger_online_ordering_backend/src/data/requests/CreateSalesReportRequest.java
@@ -1,4 +1,66 @@
 package data.requests;
 
+import data.types.Order;
+
+import java.util.List;
+import java.util.Objects;
+
 public class CreateSalesReportRequest {
+
+    private List<Order> salesReport;
+
+    public CreateSalesReportRequest() {
+
+    }
+
+    public CreateSalesReportRequest(Builder builder) {
+        this.salesReport = builder.
+
+    }
+
+    public List<Order> getSalesReport() {
+        return salesReport;
+    }
+
+    public void setSalesReport(List<Order> salesReport) {
+        this.salesReport = salesReport;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CreateSalesReportRequest that = (CreateSalesReportRequest) o;
+        return getSalesReport().equals(that.getSalesReport());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getSalesReport());
+    }
+
+    @Override
+    public String toString() {
+        return "CreateSalesReportRequest{" +
+                "salesReport=" + salesReport +
+                '}';
+    }
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    public static class Builder {
+        private List<Order> sales;
+
+
+        public Builder withSales(List<Order> withSales) {
+            this.sales = withSales;
+            return this;
+        }
+
+        public CreateSalesReportRequest build() {
+            return new CreateSalesReportRequest(this);
+        }
+    }
 }
