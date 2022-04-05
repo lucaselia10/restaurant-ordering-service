9c6c41fe3eb070269b0283f819703e6eb69078a3
William Kim <williamkim76@outlook.com>
2022-04-05 12:38:04 -0700

    Updated build.gradle and MenuItemDao

--- juicy_burger_online_ordering_backend/src/daos/MenuItemDao.java
+++ juicy_burger_online_ordering_backend/src/daos/MenuItemDao.java
@@ -25,7 +25,7 @@ public class MenuItemDao {
 
     @Inject
     public MenuItemDao(JSONParser jsonParser) {
-        this(jsonParser, "menuitems.json");
+        this(jsonParser, "menuItems.json");
     }
 
     /**
